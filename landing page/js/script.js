83fdbcf75b3175139a285952cd9db86bba036180
Daniele Dias <daniele_dias20@hotmail.com>
2021-07-18 18:11:08 -0300

    Primeiro commit

---
+++ landing page/js/script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Cadastro realizado com sucesso!')
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData);
+
+})
+
+
